b35f6f3bf5f08983b4302a2b80754e67c247c0c3
Ismail <ismail1268@mail.ru>
2023-03-12 20:25:52 +0300

    Initial commit

---
+++ main/model/Dog.java
@@ -0,0 +1,11 @@
+package main.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal{
+    @Override
+    public String toString() {
+        return "I am a dog";
+    }
+}
